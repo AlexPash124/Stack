b17ae0a56385917af9d2d8a67115770fe2112710
Ваше имя <you@example.com>
2022-04-17 11:12:20 +0300

    Добавьте файлы проекта.

---
+++ main_1/main_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Stack.h"
+
+
+int main()
+{
+	Stack A(3);
+
+	A.push(45);
+	A.push(55);
+	A.push(95);
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << A.pop()<<"\t";
+	}
+}
